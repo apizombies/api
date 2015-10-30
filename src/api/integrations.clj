8cf0a37972100180cf3100d42382f792582c2931
David Ortiz <z.david.ortiz@gmail.com>
2015-10-30 12:28:38 +0100

    Add skeleton for google accounts integration

--- src/api/integrations.clj
+++ src/api/integrations.clj
@@ -3,28 +3,44 @@
 
 (def base-url "") ;; TODO
 
+(def google-accounts-endpoint ()) ;; TODO
 (def github-endpoint (str base-url "/github"))
 (def slack-endpoint (str base-url "/slack"))
 
+(defn- google-mail [username]
+  (str username "@apizombies.lol"))
+
+(defn- add-to-google-accounts [username name]
+  ;;TODO
+  )
+
+(defn- delete-from-google-accounts [username]
+  ;;TODO
+  )
+
 (defn- add-to-github-org [github-id]
   (let [url (str github-endpoint "/" github-id)]
-    (http/post url)))
+    (http/put url)))
 
 (defn- delete-from-github-org [github-id]
   (let [url (str github-endpoint "/" github-id)]
     (http/delete url)))
 
-(defn- add-to-slack [username]
-  (let [url (str slack-endpoint "/" username)]
-    (http/post url)))
+(defn- add-to-slack [email]
+  (let [url (str slack-endpoint "/" email)]
+    (http/put url)))
 
-(defn- delete-from-slack [username]
-  (let [url (str slack-endpoint "/" username)]
+(defn- delete-from-slack [email]
+  (let [url (str slack-endpoint "/" email)]
     (http/delete url)))
 
 (defn send-notification [notification employee]
-  (case notification
-    :new-employee (do (add-to-github-org (:github employee))
-                      (add-to-slack (:username employee)))
-    :deleted-employee (do (delete-from-github-org (:github employee))
-                          (delete-from-slack (:username employee)))))
+  (let [user-gmail (google-mail (:username employee))]
+    (case notification
+      :new-employee (do (add-to-google-accounts (:username employee)
+                                                (:name employee))
+                        (add-to-github-org (:github employee))
+                        (add-to-slack user-gmail)))
+      :deleted-employee (do (delete-from-google-accounts (:username employee))
+                            (delete-from-github-org (:github employee))
+                            (delete-from-slack user-gmail))))
