bb95c84af6589e99044513cab801831d80521c1e
David Ortiz <z.david.ortiz@gmail.com>
2015-10-30 11:52:34 +0100

    Prepare requests to integration endpoints

--- src/api/integrations.clj
+++ src/api/integrations.clj
@@ -1,35 +1,30 @@
-(ns api.integrations)
+(ns api.integrations
+  (:require [org.httpkit.client :as http]))
 
 (def base-url "") ;; TODO
 
-(defn- add-to-github-org [employee]
-  (println "Adding to GitHub")
-  ;; TODO
-  ;; (http/post "http://host.com/path" options
-  ;;         (fn [{:keys [status headers body error]}] ;; asynchronous response handling
-  ;;           (if error
-  ;;             (println "Failed, exception is " error)
-  ;;             (println "Async HTTP GET: " status))))
-  )
+(def github-endpoint (str base-url "/github"))
+(def slack-endpoint (str base-url "/slack"))
 
-(defn- delete-from-github-org [employee]
-  (println "Deleting from GitHub")
-  ;; TODO
-  )
+(defn- add-to-github-org [github-id]
+  (let [url (str github-endpoint "/" github-id)]
+    (http/post url)))
 
-(defn- add-to-slack [employee]
-  (println "Adding to Slack")
-  ;; TODO
-  )
+(defn- delete-from-github-org [github-id]
+  (let [url (str github-endpoint "/" github-id)]
+    (http/delete url)))
 
-(defn- delete-from-slack [employee]
-  (println "Deleting from Slack")
-  ;; TODO
-  )
+(defn- add-to-slack [username]
+  (let [url (str slack-endpoint "/" username)]
+    (http/post url)))
+
+(defn- delete-from-slack [username]
+  (let [url (str slack-endpoint "/" username)]
+    (http/delete url)))
 
 (defn send-notification [notification employee]
   (case notification
-    :new-employee (do (add-to-github-org employee)
-                      (add-to-slack employee))
-    :deleted-employee (do (delete-from-github-org employee)
-                          (delete-from-slack employee))))
+    :new-employee (do (add-to-github-org (:github employee))
+                      (add-to-slack (:username employee)))
+    :deleted-employee (do (delete-from-github-org (:github employee))
+                          (delete-from-slack (:username employee)))))
